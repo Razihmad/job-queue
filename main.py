ac6652bfa76c6b4a6327032016a168105dd39770
Razi <mail2razi4u@gmail.com>
2026-02-15 14:18:52 +0530

    Refactor main.py and add tasks.py for job processing
    - Renamed the import of task functions from example.py to tasks.py.
    - Created separate functions for job creation, starting, and stopping the worker in main.py for better modularity.
    - Introduced tasks.py with flaky_task and always_success_task functions to handle job execution logic.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from example import always_success_task, flaky_task
+from tasks import always_success_task, flaky_task
 from in_memory_queue import JobQueue
 from job import Job
 from queue_worker import Worker
@@ -9,21 +9,34 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 logger = logging.getLogger(__name__)
 
-
-def main():
+def create_jobs():
     job_queue = JobQueue()
-    worker = Worker(job_queue)
-
-    worker.start()
-
-    job_queue.enqueue(Job(flaky_task, "job-1"))
+    job_queue.enqueue(Job(flaky_task, "job-1", countdown=5))
     job_queue.enqueue(Job(flaky_task, "job-2"))
     job_queue.enqueue(Job(always_success_task, "job-3"))
+    return job_queue
 
+def start_worker(job_queue: JobQueue):
+    worker = Worker(job_queue)
+    logger.info("Worker started")
+    worker.start()
+    return worker
+
+def stop_worker(worker: Worker):
+    worker.stop(timeout=15)
+    logger.info("Worker stopped")
+
+
+def main():
+    job_queue = create_jobs()
+
+
+    worker = start_worker(job_queue)
     time.sleep(10)  # Let jobs process
-
-    worker.stop(timeout=15)  # Optional: avoid blocking forever if a job hangs
+    stop_worker(worker)
     logger.info("All jobs processed")
 
+
 if __name__ == "__main__":
+    
     main()
